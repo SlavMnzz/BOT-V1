7ff558246673af717d21f9d5473a3b5201fdbe20
SlavMnzz <ferrjb146@gmail.com>
2023-11-26 18:56:39 +0800

    firman

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: node . --server
+db: node . --db "mongodb+srv://botwa:Jxrt6KiUNOOccDuo@cluster0.dytrn2e.mongodb.net/?retryWrites=true&w=majority" --autocleartmp --restrict
---
+++ config.js
@@ -0,0 +1,97 @@
+
+// - Ubah nomor owner dan wm di sini.
+// + Daftar apikey terlebih dahulu sebelum menggunakan svript ini link
+//https://api.betabotz.org
+//https://api.botcahx.live
+// - Jika sudah mendaftar salin key yang ada di profie dan paste di global.lann && global.btc
+// - Contoh global.btc = 'ngaksk?'//register di https://api.botcahx.live
+// - Contoh global.lann = 'Ntahla' //register key di https://api.betabotz.org
+// - Jika sudah maka lanjutkan untuk mengubah nomor owner dan wm.
+
+
+// List Key 
+// Betabotz = digunakan hampir di semua fitur kecuali linkshort
+// Botcahx  = digunakan untuk linkshort saja
+
+global.owner = ['62857059457518']  
+global.mods = ['62857059457518'] 
+global.prems = ['62857059457518']
+global.nameowner = 'XMnzzVllnz'
+global.numberowner = '62857059457518' 
+global.mail = 'fxacbapi.my.id@gmail.com' 
+global.dana = '-'
+global.pulsa = '-'
+global.gopay = '-'
+global.namebot = 'ZYKO-MD'
+global.gc = 'https://chat.whatsapp.com/ITuY4qQrA4v1TPX2f0PC4Y'
+global.web = 'https://github.com/SlavMnzz'
+global.instagram = 'https://instagram.com/prm2.0'
+global.wm = 'ZYKO BOT-MD'
+global.watermark = wm
+global.wm2 = '⫹⫺ Zyko Bot'
+global.wm3 = '© Mnzx'
+global.wm4 = 'made by ZykoBotz-MD'
+global.wait = '_*Tunggu sedang di proses...*_'
+global.eror = '_*Server Error*_'
+global.benar = 'Benar ✅\n'
+global.salah = 'Salah ❌\n'
+global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
+global.packname = 'Made with'
+global.author = 'Bot WhatsApp'
+
+//---- image thumbnail 
+global.thumb = 'https://telegra.ph/file/b60c000de1313698bcc99.jpg'
+
+//===> Apikey
+global.lann = 'AULhjWHc' //isi apikey mu https://api.betabotz.org
+global.btc = 'Auef9BSd'//Isi apikey di https://api.botcahx.live
+global.APIs = {   
+  lann: 'https://api.betabotz.org',
+  btc: 'https://api.botcahx.live'
+}
+global.APIKeys = { 
+  'https://api.betabotz.org': 'AULhjWHc' //isi apikey mu https://api.betabotz.org
+}
+
+global.multiplier = 45
+global.rpg = {
+  emoticon(string) {
+    string = string.toLowerCase()
+    let emot = {
+      exp: '✉️',
+      money: '💵',
+      potion: '🥤',
+      diamond: '💎',
+      common: '📦',
+      uncommon: '🎁',
+      mythic: '🗳️',
+      legendary: '🗃️',
+      pet: '🎁',
+      sampah: '🗑',
+      armor: '🥼',
+      sword: '⚔️',
+      kayu: '🪵',
+      batu: '🪨',
+      string: '🕸️',
+      kuda: '🐎',
+      kucing: '🐈' ,
+      anjing: '🐕',
+      petFood: '🍖',
+      gold: '👑',
+      emerald: '💚'
+    }
+    let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+    if (!results.length) return ''
+    else return emot[results[0][0]]
+  }
+}
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
